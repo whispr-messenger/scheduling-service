6133a491ace3f9ca982ab820cf6990e59d699bcc
Roadmvn <tudy.gbaguidi@epitech.eu>
2025-09-22 20:59:10 +0200

    Clean up: Remove all source files and configurations

--- src/modules/scheduler/interfaces/queue-manager.interface.ts
+++
@@ -1,7 +0,0 @@
-import { JobData } from '../../queues/interfaces/job.interface';
-import { Job } from 'bull';
-
-export interface IQueueManager {
-  addJob(jobData: JobData, delay?: number, priority?: number): Promise<Job>;
-  getAllQueueStats(): Promise<any[]>;
-}
